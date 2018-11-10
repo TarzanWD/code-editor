4415ca204a339ef56234b7243f9d2ce6d189f73f
marek.hradil <marek.hradil@staff.techfides.eu>
2018-11-10 16:45:49 +0100

    lint

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -3,7 +3,7 @@ import Editor from 'react-simple-code-editor';
 import { highlight, languages } from 'prismjs/components/prism-core';
 import 'prismjs/components/prism-clike';
 import 'prismjs/components/prism-javascript';
- 
+
 export default ({ lang, onChangeCode, code }) => {
   return (
     <div style={{ background: '#f5f2f0' }}>
