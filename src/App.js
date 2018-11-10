d1c504b2ba67c2e8c8eb60084a66c7e78935c04d
Jakub Švehla <svehl.jakub@gmail.com>
2018-11-10 14:50:51 +0100

    add socket updates;

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import React from 'react'
 import File from './components/File'
 import Editor from './components/Editor'
 import './App.css'
+import * as R from 'ramda'
+import openSocket from 'socket.io-client';
+const socket = openSocket('http://localhost:1337');
+
+const getPathWIthChildren = R.pipe(
+  R.map(item => [item, 'children']),
+  R.flatten,
+  R.init,
+)
 
 export default class App extends React.Component {
-  constructor () {
-    super()
+  
+  constructor (props) {
+    super(props)
     this.state = {
       opened: ['node_modules', 'abc', 'index.js'],
       files: {
@@ -27,6 +37,37 @@ export default class App extends React.Component {
     }
   }
 
+  componentDidMount() {
+    socket.on('onUpdateFile', data => {
+      console.log(data)
+      this.setState({
+        files: R.assocPath(
+          [...data.path, 'content'],
+          data.newContent,
+          this.state
+        )
+      })
+    })
+  }
+
+  onChangeCode = (e) => {
+    const newCode = e.target.value
+    console.log(newCode)
+    const pathWithChildren = getPathWIthChildren(this.state.opened)
+    // this.setState({
+    //   files: R.assocPath(pathWithChildren, newCode, this.state.files)
+    // })
+    socket.emit('updateFile', {
+      path: pathWithChildren,
+      newContent: newCode
+    })
+  }
+ 
+  getCurrentContent = () => {
+    const pathWithChildren = getPathWIthChildren(this.state.opened)
+    return R.path([...pathWithChildren, 'content'], this.state.files)
+  }
+
   getFile () {
     return this.state.opened.reduce((acc, part) => {
       return acc.children ? acc.children[part] : acc[part]
@@ -39,7 +80,11 @@ export default class App extends React.Component {
         <div className='files'>
           <File file={this.state.files} name='node_modules' />
         </div>
-        <Editor file={this.getFile()} />
+        <Editor
+          file={this.getFile()}
+          code={this.getCurrentContent()}
+          onChangeCode={this.onChangeCode}
+        />
       </div>
     )
   }
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,17 +1,11 @@
 import React, {useState} from 'react'
 
-export default ({ file }) => {
-  const [code, setCode] = useState(file.content)
-
-  const handleEditorChange = (e) => {
-    setCode(e.target.value)
-  }
-
+export default ({ file, onChangeCode, code }) => {
   return (
     <textarea
       className='editor'
       value={code}
-      onChange={(e) => handleEditorChange(e)}
+      onChange={onChangeCode}
     />
   )
 }
