f1ef5ea22a0ba49e1644cb6ff77855f90adbab59
marek.hradil <marek.hradil@staff.techfides.eu>
2018-11-10 16:01:12 +0100

    editor css

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,36 +1,37 @@
 import React, {useState} from 'react'
-import HighLight from 'react-syntax-highlight';
-require('highlight.js/styles/default.css');
+import HighLight from 'react-syntax-highlight'
+import glamorous from 'glamorous'
+require('highlight.js/styles/default.css')
+
+const HighLightSection = glamorous(HighLight)({
+  width: '83vw',
+  height: '90vh',
+  '& code': {
+    height: '90vh',
+  }
+})
 
 
 export default ({ onChangeCode, code }) => {
   return (
     <>
-    {/*
-      <textarea
-        className='editor'
-        value={code}
-        onChange={onChangeCode}
-      />
-      */}
-      <div className='editor' style={{marginLeft: '100px'}}>
-        <HighLight
+      <div className='editor'>
+        <HighLightSection
           lang={'javascript'}
           value={code || ''}
-          style={{
-            width: '75vw',
-            height: '60vh'
-          }}
         />
         <pre style={{
-              width: '75vw',
-              height: '60vh'
+              width: '83vw',
+              height: '90vh'
             }}>
           <code
             className='hljs textarea editor'
             contentEditable='true'
             spellCheck='false'
             onInput={onChangeCode}
+            style={{
+              height: '90vh'
+            }}
           />
         </pre>
       </div>
